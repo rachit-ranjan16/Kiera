9fd5197c1d960d506df06084b3b02b097a4d5f94
Aditya Vashist <adityavashistee@gmail.com>
2018-03-10 19:35:15 -0500

    output_labels to store the labels of data in 14 categories

--- utils/BDE-Sampler.py
+++ utils/BDE-Sampler.py
@@ -4,6 +4,7 @@
 from skimage import io
 from skimage.transform import resize
 from scipy import misc
+import numpy as np
 #from PIL import Image
 import glob
 import os
@@ -25,14 +26,12 @@ class Sampler:
         self.score = 0
 
     def process_images(self, location):
-        i = 1
+        output_labels = []
         for filename in glob.glob(location): #read all bmp files from all the folders in Real_Train folder
             im = io.imread(filename)
             img_size = self.config.get('%s' % DEEP_LEARNING, '%s' % FRAME_SIZE)
             g = resize(im, (img_size, img_size), mode='reflect') #resize the image to 64*64 size
-            saveName = 'resizeImage/' + filename[6:]
-            print(saveName)
-            misc.imsave(saveName, g)
-            i = i+1
-            #io.imshow(g)
-            #plt.show()
+            save_name = 'resizeImage/' + filename.split('/')[2]
+            output_labels.apend(int(filename.split('/')[2].split('_')[1]))
+            misc.imsave(save_name, g)
+        return np.array(output_labels)
