311c717e37c441481b7c78669c78fc0c2143170d
rachit-ranjan16 <rachit.ranjan93@gmail.com>
2018-04-22 00:02:43 -0400

    Fixes

--- rec/tasks.py
+++ rec/tasks.py
@@ -9,9 +9,14 @@ def init_learning():
     dL = DeepLearn()
     print('Initiating Deep Learning')
     # dL.init_deep_learning()
-    dL.test_save_model()
-    print('Training Completed\nModel Saved')
-    # TODO Add method in DeepLearn to save model
-    print('Updating Status')
-    with open('status_info.txt', mode='w') as f:
-        f.write('COMPLETED')
+    try:
+        dL.test_save_model()
+        print('Training Completed\nModel Saved')
+        # TODO Add method in DeepLearn to save model
+        print('Updating Status')
+        with open('status_info.txt', mode='w') as f:
+            f.write('COMPLETED')
+    except Exception as e:
+        print("Caught Exception %r" % e.with_traceback())
+        with open('status_info.txt', mode='w') as f:
+            f.write('ERROR')
